11b2325861edb884c48a930a03aaa9f1a57b60f1
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-13 00:31:09 -0300

    complete example compiles

--- osk.c
+++ osk.c
@@ -6,6 +6,9 @@
 
 #include <fcntl.h>
 #include <linux/fb.h>
+#include <linux/input.h>
+#include <linux/ioctl.h>
+#include <linux/uinput.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
@@ -16,6 +19,46 @@
 #include <unistd.h>
 
 
+/* from key codes, from /usr/include/linux/input.h
+#define KEY_Q			16
+#define KEY_W			17
+#define KEY_E			18
+#define KEY_R			19
+#define KEY_T			20
+#define KEY_Y			21
+#define KEY_U			22
+#define KEY_I			23
+#define KEY_O			24
+#define KEY_P			25
+#define KEY_LEFTBRACE		26
+#define KEY_RIGHTBRACE		27
+#define KEY_ENTER		28
+#define KEY_LEFTCTRL		29
+#define KEY_A			30
+#define KEY_S			31
+#define KEY_D			32
+#define KEY_F			33
+#define KEY_G			34
+#define KEY_H			35
+#define KEY_J			36
+#define KEY_K			37
+#define KEY_L			38
+#define KEY_SEMICOLON		39
+#define KEY_APOSTROPHE		40
+#define KEY_GRAVE		41
+#define KEY_LEFTSHIFT		42
+#define KEY_BACKSLASH		43
+#define KEY_Z			44
+#define KEY_X			45
+#define KEY_C			46
+#define KEY_V			47
+#define KEY_B			48
+#define KEY_N			49
+#define KEY_M			50
+*/
+
+
+unsigned char abc[] = {30, 48, 46, 32, 18, 33, 34, 35, 23, 36, 37, 38, 50, 49, 24, 25, 16, 19, 31, 20, 22, 47, 17, 45, 21, 44, 28};
 
 
 //kbhit, Non-blocking keypress detector, when go keypress, return 1 else always return 0
@@ -137,6 +180,24 @@ static void fb_drawimage(const char *filename, char *fbp, int xo, int yo, int bp
 
 
 
+int send_event(int fd, __u16 type, __u16 code, __s32 value)
+{
+    struct input_event event;
+
+    memset(&event, 0, sizeof(event));
+    event.type = type;
+    event.code = code;
+    event.value = value;
+
+    if (write(fd, &event, sizeof(event)) != sizeof(event)) {
+        fprintf(stderr, "Error on send_event");
+        return -1;
+    }
+
+    return 0;
+}
+
+
 
 int main(void) {
 
@@ -147,6 +208,62 @@ int main(void) {
      char *fbp = 0;
      int x = 0, y = 0;
      long int location = 0;
+
+	/* uinput init */
+    int i,fd;
+    struct uinput_user_dev device;
+    memset(&device, 0, sizeof device);
+
+    fd=open("/dev/uinput",O_WRONLY);
+    strcpy(device.name,"test mouse");
+
+    device.id.bustype=BUS_USB;
+    device.id.vendor=1;
+    device.id.product=1;
+    device.id.version=1;
+
+    for (i=0; i < ABS_MAX; i++) {
+        device.absmax[i] = -1;
+        device.absmin[i] = -1;
+        device.absfuzz[i] = -1;
+        device.absflat[i] = -1;
+    }
+
+    device.absmin[ABS_X]=0;
+    device.absmax[ABS_X]=255;
+    device.absfuzz[ABS_X]=0;
+    device.absflat[ABS_X]=0;
+
+    if (write(fd,&device,sizeof(device)) != sizeof(device))
+    {
+        fprintf(stderr, "error setup\n");
+    }
+
+    if (ioctl(fd,UI_SET_EVBIT,EV_KEY) < 0)
+        fprintf(stderr, "error evbit key\n");
+
+	for (i=0;i<27;i++)
+    //if (ioctl(fd,UI_SET_KEYBIT, KEY_A) < 0)
+    if (ioctl(fd,UI_SET_KEYBIT, abc[i]) < 0)
+        fprintf(stderr, "error evbit key\n");
+
+    if (ioctl(fd,UI_SET_EVBIT,EV_REL) < 0)
+            fprintf(stderr, "error evbit rel\n");
+
+    for(i=REL_X;i<REL_MAX;i++)
+        if (ioctl(fd,UI_SET_RELBIT,i) < 0)
+            fprintf(stderr, "error setrelbit %d\n", i);
+
+    if (ioctl(fd,UI_DEV_CREATE) < 0)
+    {
+        fprintf(stderr, "error create\n");
+    }
+
+//    i = getchar();
+
+	sleep(2);
+	/* end uinput init */
+
 
      // Open the file for reading and writing
      fbfd = open("/dev/fb0", O_RDWR);
@@ -185,7 +302,7 @@ int main(void) {
      x = 300; y = 100;       // Where we are going to put the pixel
 
     char c;
-    int i=0;
+    i=0;
     
     nonblock(NB_ENABLE);
 
@@ -194,7 +311,7 @@ int main(void) {
 
 
 
-	int j, m=0;
+	int j, m=0,k;
 	while(1) {
 		m++;
 		if (m==27) m=0;
@@ -233,20 +350,41 @@ int main(void) {
 	}
 
 	/* check for key */
+	k=48;
 	for (waiting=0; waiting<10000; waiting++) {
 	        usleep(1);
 		i=kbhit();
 		if (i!=0)
 		{
 			c=fgetc(stdin);
-			if (c==10)
-       	         		printf("%c",'a'+(m));
-				fflush(0);
+			if (c==10) {
+       	         		//printf("caracter %c",'a'+(m));
+       	         		//printf("caracter m=%i, abc=%i",m, abc[m]);
+				//fflush(0);
+				k = m;
+			}
        	 	}
 	}
+	
+	if (k!=48) {
+		send_event(fd, EV_KEY, abc[k], 1);
+		send_event(fd, EV_SYN, SYN_REPORT, 0);
+		send_event(fd, EV_KEY, abc[k], 0);
+		send_event(fd, EV_SYN, SYN_REPORT, 0);
+	}
+
+
+
 	}
 
     nonblock(NB_DISABLE);
+
+
+
+	close(fd);
+
+
+
 
      munmap(fbp, screensize);
      close(fbfd);
