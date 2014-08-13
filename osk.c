577678a0ba02792c7551894dbe7552975bbd5937
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-13 15:37:34 -0300

    fb0

--- osk.c
+++ osk.c
@@ -221,7 +221,7 @@ int main(void) {
 
 	/* init framebuffer */
 	/* Open the file for reading and writing */
-	fbfd = open("/dev/fb1", O_RDWR);
+	fbfd = open("/dev/fb0", O_RDWR);
 	if (fbfd == -1) {
 		perror("Error: cannot open framebuffer device");
 		exit(1);
