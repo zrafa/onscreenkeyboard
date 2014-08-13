d86ad48f2d5c357f6ce5a23dd5dd2eb771592504
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-13 09:26:18 -0300

    better code

--- osk.c
+++ osk.c
@@ -19,7 +19,7 @@
 #include <unistd.h>
 
 
-/* from key codes, from /usr/include/linux/input.h
+/* key codes REFERENCE : from /usr/include/linux/input.h
 #define KEY_Q			16
 #define KEY_W			17
 #define KEY_E			18
@@ -61,49 +61,9 @@
 unsigned char abc[] = {30, 48, 46, 32, 18, 33, 34, 35, 23, 36, 37, 38, 50, 49, 24, 25, 16, 19, 31, 20, 22, 47, 17, 45, 21, 44, 28};
 
 
-//kbhit, Non-blocking keypress detector, when go keypress, return 1 else always return 0
-int kbhit()
-{
-    struct timeval tv;
-    fd_set fds;
-    tv.tv_sec = 0;
-    tv.tv_usec = 0;
-    FD_ZERO(&fds);
-    FD_SET(STDIN_FILENO, &fds); //STDOUT_FILENO is 0
-    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
-    return FD_ISSET(STDIN_FILENO, &fds);
-}
-
-#define NB_DISABLE 1
-#define NB_ENABLE 0
-
-void nonblock(int state)
-{
-    struct termios ttystate;
-
-    /* get the terminal state */
-    tcgetattr(STDIN_FILENO, &ttystate);
-
-    if (state==NB_ENABLE)
-    {
-        /* turn off canonical mode */
-        ttystate.c_lflag &= ~ICANON;
-        /* minimum of number input read. */
-        ttystate.c_cc[VMIN] = 1; 
-    }
-    else if (state==NB_DISABLE)
-    {
-        /* turn on canonical mode */
-        ttystate.c_lflag |= ICANON;
-    }
-    /* set the terminal attributes. */
-    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
-
-}
 
 
-
-/**
+/*
  *	Draw image from PPM file
  */
 static void fb_drawimage(const char *filename, char *fbp, int xo, int yo, int bpp, int length) 
@@ -117,7 +77,7 @@ static void fb_drawimage(const char *filename, char *fbp, int xo, int yo, int bp
 	memset(head, 0, sizeof(head));
 	theme_file = fopen(filename, "r");
 
-	// parse ppm header
+	/* parse ppm header */
 	while (1) {
 		if (fgets(s, sizeof(s), theme_file) == NULL) {
 			printf("bad PPM file '%s'", filename);
@@ -138,15 +98,15 @@ static void fb_drawimage(const char *filename, char *fbp, int xo, int yo, int bp
 			exit(1);
 		}
 
-		// width, height, max_color_val
+		/* width, height, max_color_val */
 		if (sscanf(head, "P6 %u %u %u", &width, &height, &i) == 3)
 			break;
-// TODO: i must be <= 255!
+		/* TODO: i must be <= 255! */
 	}
 
 	line_size = width*3;
 
-     long int location = 0;
+	long int location = 0;
 	pixline = malloc(line_size);
 	for (j = 0; j < height; j++) {
 		unsigned char *pixel = pixline;
@@ -165,13 +125,10 @@ static void fb_drawimage(const char *filename, char *fbp, int xo, int yo, int bp
              location = (i+xo) * (bpp/8) +
                         (j+yo) * length;
 
-                 //*(fbp + location) = (((unsigned)pixel[0] << 8) & 0xf800);        // Some blue
-                 //*(fbp + location + 1) = (((unsigned)pixel[1] << 3) & 0x07e0);     // A little green
-                 //*(fbp + location + 2) = (((unsigned)pixel[2] >> 3));;    // A lot of red
-                 *(fbp + location) = (((unsigned)pixel[0]));        // Some blue
-                 *(fbp + location + 1) = (((unsigned)pixel[1]));     // A little green
-                 *(fbp + location + 2) = (((unsigned)pixel[2]));    // A lot of red
-                 *(fbp + location + 3) = 2;      // No transparency
+                 *(fbp + location) = (((unsigned)pixel[0]));
+                 *(fbp + location + 1) = (((unsigned)pixel[1]));
+                 *(fbp + location + 2) = (((unsigned)pixel[2]));
+                 *(fbp + location + 3) = 2;      /* No transparency */
 		}
 	}
 	free(pixline);
@@ -210,111 +167,106 @@ int main(void) {
      long int location = 0;
 
 	/* uinput init */
-    int i,fd;
-    struct uinput_user_dev device;
-    memset(&device, 0, sizeof device);
+	int i,fd;
+	struct uinput_user_dev device;
+	memset(&device, 0, sizeof device);
 
-    fd=open("/dev/uinput",O_WRONLY);
-    strcpy(device.name,"test mouse");
+	fd=open("/dev/uinput",O_WRONLY);
+	strcpy(device.name,"test mouse");
 
-    device.id.bustype=BUS_USB;
-    device.id.vendor=1;
-    device.id.product=1;
-    device.id.version=1;
+	device.id.bustype=BUS_USB;
+	device.id.vendor=1;
+	device.id.product=1;
+	device.id.version=1;
 
-    for (i=0; i < ABS_MAX; i++) {
-        device.absmax[i] = -1;
-        device.absmin[i] = -1;
-        device.absfuzz[i] = -1;
-        device.absflat[i] = -1;
-    }
+	for (i=0; i < ABS_MAX; i++) {
+		device.absmax[i] = -1;
+		device.absmin[i] = -1;
+		device.absfuzz[i] = -1;
+		device.absflat[i] = -1;
+	}
 
-    device.absmin[ABS_X]=0;
-    device.absmax[ABS_X]=255;
-    device.absfuzz[ABS_X]=0;
-    device.absflat[ABS_X]=0;
+	device.absmin[ABS_X]=0;
+	device.absmax[ABS_X]=255;
+	device.absfuzz[ABS_X]=0;
+	device.absflat[ABS_X]=0;
 
-    if (write(fd,&device,sizeof(device)) != sizeof(device))
-    {
-        fprintf(stderr, "error setup\n");
-    }
+	if (write(fd,&device,sizeof(device)) != sizeof(device))
+	{
+		fprintf(stderr, "error setup\n");
+	}
 
-    if (ioctl(fd,UI_SET_EVBIT,EV_KEY) < 0)
-        fprintf(stderr, "error evbit key\n");
+	if (ioctl(fd,UI_SET_EVBIT,EV_KEY) < 0)
+		fprintf(stderr, "error evbit key\n");
 
 	for (i=0;i<27;i++)
-    //if (ioctl(fd,UI_SET_KEYBIT, KEY_A) < 0)
-    if (ioctl(fd,UI_SET_KEYBIT, abc[i]) < 0)
-        fprintf(stderr, "error evbit key\n");
+		if (ioctl(fd,UI_SET_KEYBIT, abc[i]) < 0)
+			fprintf(stderr, "error evbit key\n");
 
-    if (ioctl(fd,UI_SET_EVBIT,EV_REL) < 0)
-            fprintf(stderr, "error evbit rel\n");
+	if (ioctl(fd,UI_SET_EVBIT,EV_REL) < 0)
+		fprintf(stderr, "error evbit rel\n");
 
-    for(i=REL_X;i<REL_MAX;i++)
-        if (ioctl(fd,UI_SET_RELBIT,i) < 0)
-            fprintf(stderr, "error setrelbit %d\n", i);
+	for(i=REL_X;i<REL_MAX;i++)
+		if (ioctl(fd,UI_SET_RELBIT,i) < 0)
+			fprintf(stderr, "error setrelbit %d\n", i);
 
-    if (ioctl(fd,UI_DEV_CREATE) < 0)
-    {
-        fprintf(stderr, "error create\n");
-    }
-
-//    i = getchar();
+	if (ioctl(fd,UI_DEV_CREATE) < 0)
+	{
+		fprintf(stderr, "error create\n");
+	}
 
 	sleep(2);
 	/* end uinput init */
 
 
-     // Open the file for reading and writing
-     fbfd = open("/dev/fb0", O_RDWR);
-     if (fbfd == -1) {
-         perror("Error: cannot open framebuffer device");
-         exit(1);
-     }
-     printf("The framebuffer device was opened successfully.\n");
+	/* Open the file for reading and writing */
+	fbfd = open("/dev/fb0", O_RDWR);
+	if (fbfd == -1) {
+		perror("Error: cannot open framebuffer device");
+		exit(1);
+	}
+	printf("The framebuffer device was opened successfully.\n");
 
-     // Get fixed screen information
-     if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
-         perror("Error reading fixed information");
-         exit(2);
-     }
+	/* Get fixed screen information */
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+		perror("Error reading fixed information");
+		exit(2);
+	}
 
-     // Get variable screen information
-     if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
-         perror("Error reading variable information");
-         exit(3);
-     }
+	/* Get variable screen information */
+	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+		perror("Error reading variable information");
+		exit(3);
+	}
 
-     printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+	printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
 
-     // Figure out the size of the screen in bytes
-     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+	/* Figure out the size of the screen in bytes */
+	screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
 
-     // Map the device to memory
-     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
+	/* Map the device to memory */
+	fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
                         fbfd, 0);
-     if ((int)fbp == -1) {
-         perror("Error: failed to map framebuffer device to memory");
-         exit(4);
-     }
-     printf("The framebuffer device was mapped to memory successfully.\n");
+	if ((int)fbp == -1) {
+		perror("Error: failed to map framebuffer device to memory");
+		exit(4);
+	}
+	printf("The framebuffer device was mapped to memory successfully.\n");
 
-     x = 300; y = 100;       // Where we are going to put the pixel
+	x = 300; y = 100;       // Where we are going to put the pixel
 
 
 	int waiting;
 
 
 	/* init event0 */
-    int fdkey;
-    fdkey = open("/dev/input/event0", O_RDONLY);
-    struct input_event evkey;
+	int fdkey;
+	fdkey = open("/dev/input/event0", O_RDONLY);
+	struct input_event evkey;
 
-int flags = fcntl(fd, F_GETFL, 0);
-int err;
-fcntl(fdkey, F_SETFL, flags | O_NONBLOCK);
-
-
+	int flags = fcntl(fd, F_GETFL, 0);
+	int err;
+	fcntl(fdkey, F_SETFL, flags | O_NONBLOCK);
 	/* end init event0 */
 
 
@@ -391,7 +343,6 @@ fcntl(fdkey, F_SETFL, flags | O_NONBLOCK);
 
 	}
 
-    nonblock(NB_DISABLE);
 
 
 
@@ -404,27 +355,4 @@ fcntl(fdkey, F_SETFL, flags | O_NONBLOCK);
      close(fbfd);
      return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
