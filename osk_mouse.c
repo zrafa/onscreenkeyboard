f3d8600117b1c5e11b2701cd9cf33dad579181da
Rafael Ignacio Zurita <rafaelignacio.zurita@gmail.com>
2014-08-20 20:44:18 -0300

    complete version working

--- osk_mouse.c
+++ osk_mouse.c
@@ -75,10 +75,130 @@
 #define KEY_B			48
 #define KEY_N			49
 #define KEY_M			50
+
+
+
+#define KEY_RESERVED		0
+#define KEY_ESC			1
+#define KEY_1			2
+#define KEY_2			3
+#define KEY_3			4
+#define KEY_4			5
+#define KEY_5			6
+#define KEY_6			7
+#define KEY_7			8
+#define KEY_8			9
+#define KEY_9			10
+#define KEY_0			11
+#define KEY_MINUS		12
+#define KEY_EQUAL		13
+#define KEY_BACKSPACE		14
+#define KEY_TAB			15
+#define KEY_Q			16
+#define KEY_W			17
+#define KEY_E			18
+#define KEY_R			19
+#define KEY_T			20
+#define KEY_Y			21
+#define KEY_U			22
+#define KEY_I			23
+#define KEY_O			24
+#define KEY_P			25
+#define KEY_LEFTBRACE		26
+#define KEY_RIGHTBRACE		27
+#define KEY_ENTER		28
+#define KEY_LEFTCTRL		29
+#define KEY_A			30
+#define KEY_S			31
+#define KEY_D			32
+#define KEY_F			33
+#define KEY_G			34
+#define KEY_H			35
+#define KEY_J			36
+#define KEY_K			37
+#define KEY_L			38
+#define KEY_SEMICOLON		39
+#define KEY_APOSTROPHE		40
+#define KEY_GRAVE		41
+#define KEY_LEFTSHIFT		42
+#define KEY_BACKSLASH		43
+#define KEY_Z			44
+#define KEY_X			45
+#define KEY_C			46
+#define KEY_V			47
+#define KEY_B			48
+#define KEY_N			49
+#define KEY_M			50
+#define KEY_COMMA		51
+#define KEY_DOT			52
+#define KEY_SLASH		53
+#define KEY_RIGHTSHIFT		54
+#define KEY_KPASTERISK		55
+#define KEY_LEFTALT		56
+#define KEY_SPACE		57
+#define KEY_CAPSLOCK		58
+#define KEY_F1			59
+#define KEY_F2			60
+#define KEY_F3			61
+#define KEY_F4			62
+#define KEY_F5			63
+#define KEY_F6			64
+#define KEY_F7			65
+#define KEY_F8			66
+#define KEY_F9			67
+#define KEY_F10			68
+#define KEY_NUMLOCK		69
+#define KEY_SCROLLLOCK		70
+#define KEY_KP7			71
+#define KEY_KP8			72
+#define KEY_KP9			73
+#define KEY_KPMINUS		74
+#define KEY_KP4			75
+#define KEY_KP5			76
+#define KEY_KP6			77
+#define KEY_KPPLUS		78
+#define KEY_KP1			79
+#define KEY_KP2			80
+#define KEY_KP3			81
+#define KEY_KP0			82
+#define KEY_KPDOT		83
+
+#define KEY_ZENKAKUHANKAKU	85
+#define KEY_102ND		86
+#define KEY_F11			87
+#define KEY_F12			88
+#define KEY_RO			89
+#define KEY_KATAKANA		90
+#define KEY_HIRAGANA		91
+#define KEY_HENKAN		92
+#define KEY_KATAKANAHIRAGANA	93
+#define KEY_MUHENKAN		94
+#define KEY_KPJPCOMMA		95
+#define KEY_KPENTER		96
+#define KEY_RIGHTCTRL		97
+#define KEY_KPSLASH		98
+#define KEY_SYSRQ		99
+#define KEY_RIGHTALT		100
+#define KEY_LINEFEED		101
+#define KEY_HOME		102
+#define KEY_UP			103
+#define KEY_PAGEUP		104
+#define KEY_LEFT		105
+#define KEY_RIGHT		106
+#define KEY_END			107
+#define KEY_DOWN		108
+#define KEY_PAGEDOWN		109
+#define KEY_INSERT		110
+#define KEY_DELETE		111
+#define KEY_MACRO		112
+#define KEY_MUTE		113
+#define KEY_VOLUMEDOWN		114
+#define KEY_VOLUMEUP		115
+
 */
 
 
-unsigned char abc[] = {30, 48, 46, 32, 18, 33, 34, 35, 23, 36, 37, 38, 50, 49, 24, 25, 16, 19, 31, 20, 22, 47, 17, 45, 21, 44, 28};
+unsigned char abc[] = {KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L, KEY_ENTER, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_DOT, KEY_ENTER, 0, KEY_SPACE, KEY_SPACE, KEY_SPACE, KEY_COMMA, KEY_DOT, KEY_DOT, KEY_DOT, KEY_BACKSPACE};
 
 
 
@@ -220,7 +340,7 @@ int main(void) {
 	if (ioctl(fd,UI_SET_EVBIT,EV_KEY) < 0)
 		fprintf(stderr, "error evbit key\n");
 
-	for (i=0;i<27;i++)
+	for (i=0;i<50;i++)
 		if (ioctl(fd,UI_SET_KEYBIT, abc[i]) < 0)
 			fprintf(stderr, "error evbit key\n");
 
@@ -307,41 +427,8 @@ int main(void) {
 
 		fb_drawimage("image_mouse.ppm", fbp, vinfo.xoffset, vinfo.yoffset, vinfo.bits_per_pixel, finfo.line_length, vinfo.xres);
 
-/*
-		for (j=0;j<54;j=j+2) {
-     		for (y = 20; y < 30; y++)
-         		for (x = 12*j; x < 12*j+8; x++) {
-
-				location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-					(y+vinfo.yoffset) * finfo.line_length;
-
-			if (vinfo.bits_per_pixel == 32) {
-				*(fbp + location) = 100;        // Some blue
-				if ((j/2)==m) {
-					*(fbp + location + 1) = 200;     // A little green
-					*(fbp + location + 2) = 100;    // A lot of red
-				} else {
-					*(fbp + location + 1) = 100;     // A little green
-					*(fbp + location + 2) = 200;    // A lot of red
-				}
-				*(fbp + location + 3) = 100;      // No transparency
-			} else  { /* assume 16bpp */
-
-/*				int b = 10;
-				int g = (x-100)/6;     // A little green
-				int r = 31-(y-100)/16;    // A lot of red
-				unsigned short int t = r<<11 | g << 5 | b;
-				*((unsigned short int*)(fbp + location)) = t;
-			}
-
-		}
-		}
-*/
-
 
 		/* check for mouse */
-
-
 
  	ret=read(fdm, &evm, sizeof(struct input_event));
 	if (ret!=-1) {
@@ -396,6 +483,7 @@ int main(void) {
 		pressed=1;
 		pressedk=(rkp*10)+ckp;
         } else if ((bLeft==0) && (pressed==1)) {
+		if (abc[pressedk] == 0) break;
 		pressed=0;
 		send_event(fd, EV_KEY, abc[pressedk], 1);
 		send_event(fd, EV_SYN, SYN_REPORT, 0);
@@ -431,32 +519,6 @@ int main(void) {
 
 
 
-/*
-		k=0;
-		err=-1;
-		while ((err==-1) && (k<500)) {
-			err = read(fdkey, &evkey, sizeof(struct input_event));
-			k++;
-			usleep(1000);
-		}
-
-		if (err!=-1) {
-			if ((evkey.type==1) && (evkey.value==0)) {
-
-				if ((m==27) || (m==28)) break;
-				send_event(fd, EV_KEY, abc[m], 1);
-				send_event(fd, EV_SYN, SYN_REPORT, 0);
-				send_event(fd, EV_KEY, abc[m], 0);
-				send_event(fd, EV_SYN, SYN_REPORT, 0);
-			} else {
-				if (m==0)
-					m=27;
-				else
-					m--;
-			}
-			printf("%i",m);
-		}
-*/
 
 
 	}
